55e343481dbd42b47c1e5ba6032424aed287328c
manfred doersam <manfred.doersam@whu.edu>
2020-04-28 12:39:12 -0600

    CRUD for Organizations

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,42 @@
+class OrganizationsController < ApplicationController
+
+  def index
+    @organizations = Organization.all
+
+  end
+
+  def show
+    @organization = Organization.find(params[:id])
+  end
+
+  def new
+    @organization = Organization.new
+  end
+
+  def create
+    @organization = Organization.new(organization_params)
+    @organization.save
+    redirect_to organization_path(@organization)
+  end
+
+  def edit
+    @organization = Organization.find(params[:id])
+  end
+
+  def update
+    @organization = Organization.find(params[:id])
+    @organization.update(organization_params)
+    redirect_to organization_path(@organization)
+  end
+
+  def destroy
+
+  end
+
+  private
+  def organization_params
+    params.require(:organization).permit(:name, :legal_id, :contact_email)
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/organizations', to: 'organizations#index', as: 'organizations'
+  get '/organizations/new', to: 'organizations#new', as:'new_organization'
+  get '/organizations/:id', to: 'organizations#show', as: 'organization'
+  post '/organizations', to: 'organizations#create'
+  get '/organizations/:id/edit', to:'organizations#edit'
+  patch '/organizations/:id', to: 'organization#update'
+  delete '/organizations/:id', to: 'organization#destroy'
+  # resources :organizations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
